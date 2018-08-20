b4de5f2287be5a49c85dadd3abec9984bb60af5f
Matheus Pereira <matheus.spereira@hotmail.com>
2018-08-20 13:01:10 -0300

    parte 1 aula 7

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -5,6 +5,6 @@ import { Component, Input } from '@angular/core';
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-    @Input() description = 'Leão';
+    @Input() description = '';
     @Input() url = '';
 }
