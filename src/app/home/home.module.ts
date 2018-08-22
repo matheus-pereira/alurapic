917eb7f72f6d6ac6669e00efdadc6e707185fbda
matheus-pereira <matheus.spereira@hotmail.com>
2018-08-21 21:42:05 -0300

    parte 2 aula 4

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
-import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [SignInComponent],
+    declarations: [
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
